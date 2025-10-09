e9cba2dcddd323547abcf0d28ccdf160082ae2be
Gabin Fouquet <40137347+GabinFqt@users.noreply.github.com>
2025-10-09 17:55:32 +0200

    feat: better feur (#15)

--- src/handlers/quoiHandler.ts
+++ src/handlers/quoiHandler.ts
@@ -1,7 +1,11 @@
 import { Message } from 'discord.js'
 
 export function handleQuoi(message: Message) {
-	if (message.content.toLowerCase() === 'quoi') {
+	const content = message.content.toLowerCase()
+	// Regex pour matcher "quoi" avec n'importe quoi avant et des caractères non-alphanumériques après
+	const quoiRegex = /.*quoi[^a-zA-Z0-9]*$/
+	
+	if (quoiRegex.test(content)) {
 		message.reply('feur!')
 	}
 }
